d410ef95fa9b30b6e3ae206c52cc43040b453319
Betelhemf567 <b.chelebo@alustudent.com>
2025-01-25 07:21:31 -0800

    adding file

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function checkArguments() {
+	if (arguments.length === 0) {
+		console.log('No Argument');
+	} else if (arguments.length === 1) {
+		console.log('Argument found');
+	}else {
+		console.log('Argument found');
+	}
+}
+
