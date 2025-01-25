0a47494900276c8b4717b31c3eb899c0f7bef006
Betelhemf567 <b.chelebo@alustudent.com>
2025-01-25 09:01:36 -0800

    adding file

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const messages['C is fun','Python is cool','Javascript is amazing'];
+
+const messages[
+'C is fun',
+'Python is cool',
+'Javascript is amazing'
+];
 for (const message of messages) {
 	console.log(message);
 }
