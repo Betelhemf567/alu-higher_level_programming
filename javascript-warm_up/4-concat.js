538450d5a6f461bc0cf3c23b884a0cbc408cad7d
Betelhemf567 <b.chelebo@alustudent.com>
2025-01-25 08:25:18 -0800

    add file

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArgs = process.argv.slice(2)
+if (myArgs.length === 2) {
+	console.log(`${myArgs[0]} is ${myArgs[1]}`);
+} else {
+	console.log('Please provide exactly two arguments');
+}
