9d687d0199984fa54e8c9f82d8fd80268f45e2a4
Betelhemf567 <b.chelebo@alustudent.com>
2025-01-25 07:43:39 -0800

    add

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-
-function checkArguments () {
-  if (myArgs.length === 0) {
-    console.log('No Argument');
-  } else if (myArgs.length === 1) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+if (myArgs.length === 0) {
+  console.log('No argument');
+} else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
