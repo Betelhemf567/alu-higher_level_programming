36d10c1843a5ea793536be61bb49e68d41c88d0b
Betelhemf567 <b.chelebo@alustudent.com>
2025-01-25 08:01:35 -0800

    adding the file

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-if (myArg.length > 0) {
-	console.log(myArg[0]);
+if (myArgs[0]) {
+	console.log(myArgs[0]);
 } else {
 	console.log('No argument provided');
 }
